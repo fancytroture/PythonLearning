84bf24dc658ec38b0d89122d4d8328c187af7b47
fancytroture <2420576957@qq.com>
2025-04-17 22:43:10 +0800

    2nd change

---
+++ 2nd/2_3.py
@@ -0,0 +1,7 @@
+###title
+name = "ada lovelace"
+print(name.title())
+
+###upper and lower
+print(name.upper())
+print(name.lower())
---
+++ 2nd/simple_messages.py
@@ -0,0 +1,4 @@
+message = "I love zuoxin!"
+print(message)
+message = "I love zuoxin very much!"
+print(message)
