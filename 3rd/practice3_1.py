2d05febbddbd697c508e16821c352bd473dcf309
XuezhenCAI <2420576957@qq.com>
2025-04-23 14:53:54 +0800

    practice3_1

---
+++ 3rd/practice3_1.py
@@ -0,0 +1,19 @@
+##friend names
+friend_names = ['huyuzhe', 'zhaokewei', 'sunyapeng', 'sjb']
+print(friend_names[0])
+print(friend_names[1])
+print(friend_names[2])
+print(friend_names[3])
+
+##conbine message
+friend_names = ['huyuzhe', 'zhaokewei', 'sunyapeng', 'sjb']
+print(f"Hello, {friend_names[0].title()}")
+print(f"Hello, {friend_names[1].title()}")
+print(f"Hello, {friend_names[2].title()}")
+print(f"Hello, {friend_names[3].title()}")
+
+## My favourite tran
+brands = ['Audi', 'Benz', 'toyota', 'nissan']
+cars = ['car', 'suv']
+message = f"My favourite car is {brands[1].title()} {cars[-1].lower()}!"
+print(message)
