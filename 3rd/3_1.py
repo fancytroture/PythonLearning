1035b5c47546e0620695cbd3cffd381ae03ee7d5
XuezhenCAI <2420576957@qq.com>
2025-04-23 14:15:41 +0800

    3rd start

---
+++ 3rd/3_1.py
@@ -0,0 +1,22 @@
+bicycle = ['shanghai', 'beijing', 'sichuan']
+print(bicycle)
+
+##index and visit element
+bicycle = ['shanghai', 'beijing', 'sichuan']
+print(bicycle[0])
+
+##conbination
+bicycle = ['shanghai', 'beijing', 'sichuan']
+print(bicycle[0].title())
+
+##index number problem
+bicycle = ['shanghai', 'beijing', 'sichuan']
+print(bicycle[1].upper())
+print(bicycle[-1].lower())
+
+##message combine
+lover = ['zuo xin', 'jerry', 'tom', 'nailong']
+message = f"My favourite carteen character is {lover[-1].title()}. \
+            However, {lover[0].strip()} is another choice."
+print(message)
+
