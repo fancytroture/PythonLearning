b2c0286fcd6349b9908530eb64d5fe28a04fd093
fancytroture <2420576957@qq.com>
2025-04-26 00:56:53 +0800

    finish 3

---
+++ 3rd/practice3_3.py
@@ -0,0 +1,58 @@
+##five cities wanted to tour
+city = ["chengdu", "shanghai", "jilin", "taibei", "qingdao"]
+print(city)
+print(sorted(city))
+print(city)
+print(sorted(city,reverse=True))
+print(city)
+city.reverse()
+print(city)
+print(city)
+print(city.reverse())
+print(city)
+print(city.sort())
+print(city)
+print(city.sort(reverse=True))
+print(city)
+###note: don't use print(city.reverse()), because for the function there is no return
+
+##dinner
+list = ['kobe', 'curry', 'messi']
+
+miss_person = list.pop(0)
+print(f"{miss_person.title()}")
+list.insert(0,'joker')
+print(list)
+
+list.insert(0,'mama')
+list.insert(2,'zuoxin')
+list.append('durant')
+print(list)
+#miss_person = list[0]
+#print(miss_person)
+#list[0]=joker
+#print(list)
+
+print(f"Hello {list[0].title()}, I would like to invite you to have a dinner.")
+print(f"Hello {list[1].title()}, I would like to invite you to have a dinner.")
+print(f"Hello {list[2].title()}, I would like to invite you to have a dinner.")
+print(f"Hello {list[3].title()}, I would like to invite you to have a dinner.")
+print(f"Hello {list[4].title()}, I would like to invite you to have a dinner.")
+print(f"Hello {list[5].title()}, I would like to invite you to have a dinner.")
+print("I am sorry to tell you that I can only invite two guests due to limited dishes.")
+popped_person = list.pop()
+print(f"I am very sorry, {popped_person.title()}.")
+popped_person = list.pop()
+print(f"I am very sorry, {popped_person.title()}.")
+popped_person = list.pop()
+print(f"I am very sorry, {popped_person.title()}.")
+popped_person = list.pop(1)
+print(f"I am very sorry, {popped_person.title()}.")
+print(list)
+print(f"{list[0].title()}, you are still invited!")
+print(f"{list[1].title()}, you are still invited!")
+print(len(list))
+del list[0]
+del list[0]
+print(list)
+print(len(list))
